8b141c13555aca0bbbc4c76b9c4e584eb932be09
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2024-09-29 18:56:48 +0530

    updated-navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import WebLayout from './layouts/WebLayout'
+import Projects from './pages/Projects'
 
 export const App = () => {
   return (
@@ -14,6 +15,7 @@ export const App = () => {
               <Route path='/' element = { <Home />} />
               <Route path='/about' element = { <About />} />
               <Route path='/contact' element = { <Contact />} />
+              <Route path='/projects' element = { <Projects />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- portfolio/src/component/Navbar.jsx
+++ portfolio/src/component/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import '../assets/css/style.css'
-import { Home, Search } from 'lucide-react'
+import { Home, Search, User } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
+import Resume from '../assets/pdf/Resume.pdf'
 
 export const Navbar = () => {
+
+  const handleResumeButton = () => {
+    const link = document.createElement('a')
+    link.href = Resume
+    link.setAttribute('download', 'Resume.pdf')
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
 
   const NavLinks = [ 
     {
@@ -15,6 +26,10 @@ export const Navbar = () => {
       url:"./about"
     },
     {
+      title: "Projects",
+      url:"./projects"
+    },
+    {
       title:"Contact",
       url:"./contact"
     }
@@ -23,15 +38,15 @@ export const Navbar = () => {
     <>
       <div className='w-full h-[10vh] bg-[#d1f2eb] shadow-md flex justify-center items-center'>
         <div className='w-[98%] h-[80%] flex justify-start items-center flex-row gap-[3.5rem]'>
-          <div className='w-2/12 h-full flex justify-center items-center font-bold text-black text-4xl '><Home className='w-10 h-10 text-red-500 font-bold' />Hunt</div>
+          <div className='w-4/12 h-full flex justify-center items-center font-bold text-black text-4xl '><User className='w-10 h-10 text-red-500 font-bold' />Vasantha Krishnan S</div>
           <div className='w-3/12 h-full flex justify-center items-center'>
             <form className=' w-full h-full flex justify-center items-center flex-row search-form'>
             <input type="search" name="" id="" placeholder='Search Here ...' className='search'/>
             <Search className='w-7 h-7 search-icon' />
             </form>
           </div>
-          <div className='w-6/12 h-full flex justify-center items-center'>
-            <ul className='w-[90%] h-full flex justify-evenly items-center flex-row'>
+          <div className='w-4/12 h-full flex justify-center items-center'>
+            <ul className='w-[90%] h-full flex justify-center items-center flex-row gap-5'>
               {
                 NavLinks.map((navdata, index) => (
                   <NavLink key={index} to={navdata.url} className='border-b-4 border-transparent w-[5rem]'>
@@ -41,8 +56,8 @@ export const Navbar = () => {
               }
             </ul>
           </div>
-          <div className='w-1/12 h-full flex justify-center items-center'>
-            <button className='px-4 py-2.5 rounded-md font-bold text-2xl bg-black text-white'>Login</button>
+          <div className='w-2/12 h-full flex justify-center items-center'>
+            <button onClick={ handleResumeButton } className='px-4 py-2.5 rounded-md font-bold text-2xl bg-black text-white'>Download CV</button>
           </div>
         </div>
       </div>
